2487dfde995ba833209a8c62b26b04ba67f9559e
zhangmingfei0214 <2960093908@qq.com>
2018-08-06 10:13:37 +0800

    测试idea的git方法

---
+++ src/com/zhx/TestIdea.java
@@ -0,0 +1,10 @@
+package com.zhx;
+
+/**
+ * Created by Administrator on 2018/8/6.
+ */
+public class TestIdea {
+    public static void main(String[] args) {
+        System.out.println("请输入Idea的版本：");
+    }
+}
